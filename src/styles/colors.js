6fb5d7ee6418962d1c83082d94a0d85f4c4ff527
Filipe Pires <filipepiresg@gmail.com>
2021-02-24 21:06:36 -0300

    chore: styles

---
+++ src/styles/colors.js
@@ -0,0 +1,6 @@
+export default {
+  PRIMARY: '#FFF',
+  SECONDARY: '#25C0C5',
+  BLACK: '#000',
+  BACKGROUND: '#F7F7F7',
+};
---
+++ src/styles/metrics.js
@@ -0,0 +1,9 @@
+import {Dimensions} from 'react-native';
+
+const {height, width} = Dimensions.get('screen');
+
+export default {
+  SCREEN_HEIGHT: height,
+  SCREEN_WIDTH: width,
+  PADDING: 10,
+};
